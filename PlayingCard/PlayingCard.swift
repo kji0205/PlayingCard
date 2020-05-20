557a0a25fb2bfac0bcbf7c0e30e47264c2efcc42
jimmy <kji0205@gmail.com>
2020-05-20 21:59:08 +0900

    UIView, Draw, Content Mode, Contraints, Aspect ratio,@IBDesignable, @IBInspectable

---
+++ PlayingCard/PlayingCard.swift
@@ -0,0 +1,57 @@
+//
+//  PlayingCard.swift
+//  PlayingCard
+//
+//  Created by jimmy on 2020/05/19.
+//  Copyright © 2020 Jimmy. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCard: CustomStringConvertible {
+    
+    var description: String { return "\(rank)\(suit)" }
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String, CustomStringConvertible {
+        
+        case spades = "♠️"
+        case hearts = "❤️"
+        case clubs = "♣️"
+        case diamonds = "♦️"
+        
+        static var all = [Suit.spades,.hearts,.diamonds,.clubs]
+        
+        var description: String { return rawValue }
+    }
+    
+    enum Rank: CustomStringConvertible {
+        var description: String { return "\(order)" }
+        
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+                case .face(let kind) where kind == "Q": return 11
+                case .face(let kind) where kind == "K": return 11
+            default: return 0
+            }
+        }
+        
+        static var all: [Rank] {
+            var allRank: [Rank] = [Rank.ace]
+            for pips in 2...10 {
+                allRank.append(Rank.numeric(pips))
+            }
+            allRank += [Rank.face("J"),Rank.face("Q"),Rank.face("K")]
+            return allRank
+        }
+    }
+}
